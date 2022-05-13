a31cf6f21df86033a406f5f93bda941fe9228cf1
aTorrecillas <a.torrecillas@alumnos.upm.es>
2022-05-13 20:45:54 +0200

    Added Song Entity and Song Repository #7

---
+++ src/Entity/Song.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SongRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SongRepository::class)]
+class Song
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 60)]
+    private string $title;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $lyrics;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLyrics(): ?string
+    {
+        return $this->lyrics;
+    }
+
+    public function setLyrics(?string $lyrics): self
+    {
+        $this->lyrics = $lyrics;
+
+        return $this;
+    }
+}
