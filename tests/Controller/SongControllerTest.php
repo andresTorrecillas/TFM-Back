040ac77811ffe4d329054509829d35cdbb4b7c20
aTorrecillas <a.torrecillas@alumnos.upm.es>
2022-05-18 13:41:05 +0200

    Updated SongController test (created unitary and integration test) and class #7

--- tests/Controller/SongControllerTest.php
+++ tests/Controller/SongControllerTest.php
@@ -2,45 +2,52 @@
 
 namespace App\Tests\Controller;
 
-use App\Entity\Song;
 use App\Controller\SongController;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Song;
+use App\Repository\SongRepository;
 use Faker\Factory as FakerFactory;
 use Faker\Generator as FakerGenerator;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class SongControllerTest extends WebTestCase
+class SongControllerTest extends TestCase
 {
+    private MockObject $SongRepositoryMock;
+    private ManagerRegistry $mockedOrm;
     private static FakerGenerator $faker;
-    private static KernelBrowser $client;
 
     public static function setUpBeforeClass(): void
     {
-        self::$client = static::createClient();
         self::$faker = FakerFactory::create('es_ES');
+    }
+
+    protected function setUp(): void
+    {
+        $this->SongRepositoryMock = $this->createMock(SongRepository::class);
+        $this->mockedOrm = $this->createMock(ManagerRegistry::class);
+        $this->mockedOrm->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($this->SongRepositoryMock);
     }
 
     public function testCreateSong()
     {
         $song = new Song();
         $song->setTitle(self::$faker->sentence(3))->setLyrics(self::$faker->text());
-        self::$client->request(
-            Request::METHOD_POST,
-            SongController::ROOT_PATH,
-            [],
-            [],
-            [],
-            json_encode($song)
-        );
-        $response = self::$client->getResponse();
-        $receivedSong = json_decode($response->getContent(),true);
-        self::assertResponseIsSuccessful();
-        self::assertJson($response->getContent());
-        self::assertSame($song->getTitle(), $receivedSong["title"]);
-        self::assertSame($song->getLyrics(), $receivedSong["lyrics"]);
+        $songController = new SongController();
+        $this->SongRepositoryMock->expects($this->any())
+            ->method('add');
+        $response = $songController->createSong(new Request(content:json_encode($song)), $this->mockedOrm);
 
-
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $receivedSong = json_decode($response->getContent(), true);
+        echo $response->getContent();
+        $this->assertSame($song->getTitle(), $receivedSong['title']);
+        $this->assertSame($song->getLyrics(), $receivedSong['lyrics']);
     }
 }
