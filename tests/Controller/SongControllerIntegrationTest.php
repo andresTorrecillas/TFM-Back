832a42da25020b00463fe6a2a6760ae457071183
aTorrecillas <a.torrecillas@alumnos.upm.es>
2022-06-06 11:25:41 +0200

    Updated Integration test #8

--- tests/Controller/SongControllerIntegrationTest.php
+++ tests/Controller/SongControllerIntegrationTest.php
@@ -43,7 +43,8 @@ class SongControllerIntegrationTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertJson($response->getContent());
         $receivedSong = json_decode($response->getContent(), true);
-        self::assertEquals($id, $receivedSong["id"]);
+        //self::assertEquals($id, $receivedSong["id"]);
+        $this->assertEquals($id, $receivedSong["id"]);
         self::assertArrayHasKey("title", $receivedSong);
         self::assertArrayHasKey("lyrics", $receivedSong);
     }
